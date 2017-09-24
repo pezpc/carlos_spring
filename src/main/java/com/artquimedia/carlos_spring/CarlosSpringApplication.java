69bffab080ab3b654309369b4e4ee9077ef7ad76
Pedro Badenes <pedro@badenes.org>
2017-09-24 13:34:16 -0400

    Initial commit

---
+++ src/main/java/com/artquimedia/carlos_spring/CarlosSpringApplication.java
@@ -0,0 +1,12 @@
+package com.artquimedia.carlos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlosSpringApplication.class, args);
+	}
+}
